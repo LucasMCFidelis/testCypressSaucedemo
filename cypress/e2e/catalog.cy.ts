8fc4a810bd546ad2c2a8f209017e0a2d199cac3f
Lucas Fidelis <lucasm241301@gmail.com>
2025-04-29 19:38:41 -0300

    Adiciona testes de ordenação da lista de produtos

---
+++ cypress/e2e/catalog.cy.ts
@@ -0,0 +1,92 @@
+function getProductTitles($products: JQuery<HTMLElement>) {
+  return [...$products].map(product =>
+    product.querySelector('[data-test="inventory-item-name"]')?.textContent?.trim() || ""
+  );
+}
+
+function getProductPrices($products: JQuery<HTMLElement>) {
+  return [...$products].map((product) => {
+    const priceText = product
+      .querySelector('[data-test="inventory-item-price"]')
+      ?.textContent?.trim()
+      .replace("$", "");
+  
+    return priceText ? parseFloat(priceText) : 0;
+  });  
+}
+
+describe("Ordenação de produtos - ", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.fillLogin({
+      username: Cypress.env("userNameValid"),
+      password: Cypress.env("passwordValid"),
+    });
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="inventory-container"]').should("be.visible");
+  });
+
+  it("ordenar por ordem alfabética crescente (A → Z)", () => {
+    cy.get('[data-test="product-sort-container"]').should("exist").select("az");
+
+    cy.wait(1000);
+
+    cy.get('[data-test="inventory-container"]')
+      .find('[data-test="inventory-item"]')
+      .then(($products) => {
+        const titles = getProductTitles($products)
+
+        const ordenado = [...titles].sort((a, b) => a.localeCompare(b));
+        expect(titles).to.deep.equal(ordenado);
+      });
+  });
+
+  it("ordenar por ordem alfabética decrescente (Z → A)", () => {
+    cy.get('[data-test="product-sort-container"]').should("exist").select("za");
+
+    cy.wait(1000);
+
+    cy.get('[data-test="inventory-container"]')
+      .find('[data-test="inventory-item"]')
+      .then(($products) => {
+        const titles = getProductTitles($products)
+
+        const ordered = [...titles].sort((a, b) => b.localeCompare(a));
+        expect(titles).to.deep.equal(ordered);
+      });
+  });
+
+  it("ordenar por menor preço (Menor → Maior)", () => {
+    cy.get('[data-test="product-sort-container"]')
+      .should("exist")
+      .select("lohi");
+
+    cy.wait(1000);
+
+    cy.get('[data-test="inventory-container"]')
+      .find('[data-test="inventory-item"]')
+      .then(($products) => {
+        const prices = getProductPrices($products)
+
+        const ordered = [...prices].sort((a, b) => a - b);
+        expect(prices).to.deep.equal(ordered);
+      });
+  });
+
+  it("ordenar por maior preço (Maior → Menor)", () => {
+    cy.get('[data-test="product-sort-container"]')
+      .should("exist")
+      .select("hilo");
+
+    cy.wait(1000);
+
+    cy.get('[data-test="inventory-container"]')
+      .find('[data-test="inventory-item"]')
+      .then(($products) => {
+        const prices = getProductPrices($products)
+
+        const ordered = [...prices].sort((a, b) => b - a);
+        expect(prices).to.deep.equal(ordered);
+      });
+  });
+});
